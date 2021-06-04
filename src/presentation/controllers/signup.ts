afc843428c6ade84c258c7909d568ebc9f654c22
AbilioN <netobalby@gmail.com>
2021-06-03 22:23:46 -0300

    feat: ensure SignUpController validate email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,30 @@
 import { HttpResponse , HttpRequest} from '../protocols/http'
-import { MissingParamError} from '../errors/missing-error'
+import { MissingParamError} from '../errors/missing-param-error'
+import { InvalidParamError} from '../errors/invalid-param-error'
+import {EmailValidator } from '../protocols/email-validator'
+
 import { badRequest } from '../helpers/http-helper'
 
 
 export class SignUpController {
+    private readonly emailValidator: EmailValidator
+    constructor(emailValidator: EmailValidator){
+        this.emailValidator = emailValidator
+    }
+
     handle (httpRequest: HttpRequest) : HttpResponse {
         const requiredFields = ['name' , 'email', 'password' , 'password_confirmation']
-
+        
         for (const field of requiredFields){
             if (!httpRequest.body[field]){
                 return badRequest(new MissingParamError(field))
             }
+        }
+
+        const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
+
+        if(!isValidEmail){
+            return badRequest(new InvalidParamError('email'))
         }
     }
 }
