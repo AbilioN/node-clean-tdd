39ca6205cfde5ad72756601eb893417afdc83d7b
AbilioN <netobalby@gmail.com>
2021-06-03 21:10:08 -0300

    test: added SignUpController Factory

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 import { SignUpController} from './signup'
 import  { MissingParamError} from '../errors/missing-error'
 
+const makeSut = (): SignUpController => {
+    return new SignUpController()
+}
+
 describe('SignUp Controller', () => {
     test('SignUp requires name', () => {
-        const sut = new SignUpController();
+        const sut = makeSut();
         const httpRequest = {
             body: {
                 email: 'any_email',
